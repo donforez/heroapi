a2851b5e3ede416a12063160a119c4652a8db525
donforez <dominik.bilinski05@gmail.com>
2024-02-27 23:26:39 +0100

    Moved assets folder, added react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import HeroesFeatured from './Components/HeroFeatured/HeroesFeatured';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <>
-      <Nav></Nav>
-      <main>
-        <div className="container">
-          <HeroesFeatured />
-        </div>
-      </main>
-      <footer></footer>
+    <Router>
+        <Nav />
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HeroesFeatured />} />
+              <Route path="/search/:name" />
+              <Route path="/hero/:id" />
+            </Routes>
+          </div>
+        </main>
+        <footer>
+          <div className="container">
+            <p>This content is kindly provided by <a href="https://superheroapi.com/">Superhero API</a></p>
+          </div>
+        </footer>
+      </Router>
     </>
   );
 }
